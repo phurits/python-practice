b012b3fbda30ee47c07e8921eaccded03e849fb3
phurits <phurits@hotmail.com>
2022-10-25 02:06:45 +0700

    plusOne

---
+++ plusone.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def plusOne(self, digits):
+        text = ""
+        for i in digits:
+            text += str(i)
+        nums = int(text)
+        nums += 1
+        text = str(nums)
+        ans = []
+        for i in range(len(text)):
+            ans.append(int(text[i]))
+        return ans
+
+
+digits = [int(x) for x in input().split()]
+
+ans = Solution().plusOne(digits)
+print(ans)
